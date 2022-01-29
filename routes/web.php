389a27d4c04458d43d640cbc47002f99954adc93
qunqfat <malachowiak@wp.pl>
2022-01-29 14:43:15 +0100

    first time with postman and routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}', 'HelloController@hello');
+
+Route::get('/goodbye/{name}', function (string $name) {
+    return 'Goodbye: ' . $name;
+});
+
+// Route::get('/example', function () {
+//     return "jestem GET";
+// });
+
+$uri = '/example';
+Route::get($uri, fn () => 'jestem arrow GET');
+Route::post($uri, fn () => 'jestem post');
+Route::put($uri, fn () => 'jestem PUT');
+Route::delete($uri, fn () => 'jestem DELETE');
+Route::patch($uri, fn () => 'jestem PATH');
+Route::options($uri, fn () => 'jestem OPTIONS');
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'jestem getem i postem jednoczesnie';
+});
+
+Route::any('/all', fn () => 'wszystkie metody');
+
+//metody HTTP
+// get - pobiera dane z serwera i zwrócenie użytkownikowi, po stronie aplikacji nie powinno się nic zmieniać
+// post - po stronie serwisu tworzy się obiekt np dodanie nowej notatki przez użytkownika
+// patch - zaktualizowanie danego pola /pól obiektu np aktualizowanie pole "tekst" w nowej notatce
+// put - aktualizowanie całego obiektu wcześniej utworzonego
+//delete - usuwanie np notatka użytkownika
+// options - zwraca opcje powiązane z danych URL/zasobem
+//match - definiuje kod który ma się wykonać zarówno dla metod get i post przy podanym URL
+//any - dopuszczamy wykonanie wszystkich metod HTTP
+// TODO: poczytać o REST API
